e83d44b8a284db23fa227530aa07ffd99a178055
Richie314 <richieinleghorn@gmail.com>
2023-12-27 17:33:11 +0100

    DB connection handling

--- utils/db.php
+++ utils/db.php
@@ -1,2 +1,25 @@
 <?php
+include_once "./string.php";
 
+$parts = parse_ini_file(".env"); # Credentials are stored in a .env file
+if (!isset($parts) || 
+    isEmpty($parts["MYSQL_USER"]) || 
+    isEmpty($parts["MYSQL_PASSWORD"]) || 
+    isEmpty($parts["MYSQL_DATABASE_NAME"]) || 
+    isEmpty($parts["MYSQL_HOST"]))
+{
+    unset($parts); # Prevent credential leaks
+    die(500);
+}
+$db = new mysqli(
+    $parts["MYSQL_HOST"], 
+    $parts["MYSQL_USER"],
+    isEmpty($parts["MYSQL_PASSWORD"]) ? null : $parts["MYSQL_PASSWORD"],
+    $parts["MYSQL_DATABASE_NAME"]);
+
+    unset($parts); # Prevent credential leaks
+if (!$db || $db->connect_errno)
+{
+    die(500);
+}
+$db->set_charset("utf-8");
