efa9f868e554db1513a6362a9b2637f3368691c0
Richie314 <richieinleghorn@gmail.com>
2024-01-12 18:53:46 +0100

    Moved style css

--- parts/stars.php
+++ parts/stars.php
@@ -1,19 +1,3 @@
-<style type="text/css">  
-    .star {
-        width: 1.5px;
-        height: 1.5px;
-        background-color: rgba(249, 251, 242, 0.8);
-        z-index: -1;
-    }
-
-        .star.star-big {
-            background-color: rgba(249, 251, 242, 0.8);
-            border: none;
-            width: 4px;
-            height: 4px;
-            border-radius: 2px;
-        }
-</style>
 <?php 
     require_once "./utils/random.php";
     if (!isset($MAX_STARS) || !is_int($MAX_STARS))
@@ -23,14 +7,11 @@
     for ($i = 0; $i < $MAX_STARS; $i++) { 
         if (random_float() > 0.94) {
 ?>
-    <div class="star star-big"
-                style="top: <?= random_percentage() ?>; left: <?= random_percentage() ?>"></div>
-    
+    <div class="star star-big" style="top: <?= random_percentage() ?>; left: <?= random_percentage() ?>"></div>
 <?php 
         } else {
 ?>
-    <div class="star"
-                style="top: <?= random_percentage() ?>; left: <?= random_percentage() ?>"></div>
+    <div class="star" style="top: <?= random_percentage() ?>; left: <?= random_percentage() ?>"></div>
 <?php
         }
     }
