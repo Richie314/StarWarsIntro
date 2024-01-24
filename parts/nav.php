06ef2bbe15fa0afe4c4234dfbb1a5320e86e2d47
Richie314 <richieinleghorn@gmail.com>
2024-01-24 17:43:29 +0100

    Don't use inverse aspect ratio

--- parts/nav.php
+++ parts/nav.php
@@ -25,13 +25,13 @@
 <nav class="h-menu items-<?= sizeof($links) ?>">
     <input type="checkbox" id="hide-show-menu">
     <label for="hide-show-menu">
-        <img src="./assets/img/hamburger.svg" alt="Hamburger" title="Mostra il menù" class="hamburger" data-inverse-aspect-ratio="1">
-        <img src="./assets/img/close.svg" alt="Chiudi" title="Chiudi il menù" class="close" data-inverse-aspect-ratio="1">
+        <img src="./assets/img/hamburger.svg" alt="Hamburger" title="Mostra il menù" class="hamburger">
+        <img src="./assets/img/close.svg" alt="Chiudi" title="Chiudi il menù" class="close">
     </label>
     <?php foreach ($links as $link) { ?>
         <a href="<?= $link->Url ?>" target="_self" title="Vai a <?= $link->Text ?>">
             <div class="icon">
-                <img src="<?= $link->Img ?>" alt="<?= $link->Text ?>" loading="eager" data-inverse-aspect-ratio="1">
+                <img src="<?= $link->Img ?>" alt="<?= $link->Text ?>" loading="eager">
             </div>
             <div class="text">
                 <span>
