fece8af876ffd2b401d66ed5f8f90449d628eb63
Richie314 <richieinleghorn@gmail.com>
2024-12-27 10:23:38 +0100

    Handle env vars

--- utils/db.php
+++ utils/db.php
@@ -1,20 +1,20 @@
 <?php
 include_once "./utils/string.php";
 
-$parts = parse_ini_file(".env"); # Credentials are stored in a .ini file
+# $parts = parse_ini_file(".env"); # Credentials are stored in a .ini file
 if (!$parts ||
-    isEmpty($parts["MYSQL_USER"]) ||  
-    isEmpty($parts["MYSQL_DATABASE_NAME"]) || 
-    isEmpty($parts["MYSQL_HOST"]))
+    isEmpty(getenv("MYSQL_USER")) ||  
+    isEmpty(getenv("MYSQL_DATABASE_NAME")) || 
+    isEmpty(getenv("MYSQL_HOST")))
 {
     unset($parts); # Prevent credential leaks
     throw new RuntimeException('Could not load db credentials', 500);
 }
 $db = new mysqli(
-    $parts["MYSQL_HOST"], 
-    $parts["MYSQL_USER"],
-    isEmpty($parts["MYSQL_PASSWORD"]) ? null : $parts["MYSQL_PASSWORD"],
-    $parts["MYSQL_DATABASE_NAME"]);
+    getenv("MYSQL_HOST"), 
+    getenv("MYSQL_USER"),
+    isEmpty(getenv("MYSQL_PASSWORD")) ? null : getenv("MYSQL_PASSWORD"),
+    getenv("MYSQL_DATABASE_NAME"));
 
 unset($parts); # Prevent credential leaks
 if (!$db || $db->connect_errno)
